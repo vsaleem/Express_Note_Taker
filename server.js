ab48e3e93800fb0ac4ffd085d5be14790426a658
vsaleem <vic.saleem@gmail.com>
2020-04-24 22:24:06 -0400

    Added express modules, connected routes, and added PORT.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // basic server template
 // see 14-FinalStarwarsApp/server-template.js
+const express = require("express");
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 // plus require routes 
+const PORT = process.env.PORT || 8080;
 
 // PLUS the following 
 app.use(express.static("public"));
@@ -14,3 +20,6 @@ to the /public directory automatically.
 app.use("/api", apiRoutes);  // /api/notes
 app.use("/", htmlRoutes);    // /notes or /
 
+app.listen(PORT, function(){
+    console.log("Listening on PORT: " + PORT);
+})
